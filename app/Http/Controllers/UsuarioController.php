aec310dbffdf56d5c3ef1c48f084aae7be6a3e74
cchura94 <cchura.cpc@gmail.com>
2022-10-17 22:28:30 -0400

    CRUD Usuario

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $usuarios = User::orderBy('id', 'desc')->get();
+
+        return response()->json($usuarios, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // validar
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required"
+        ]);
+
+        // guardar
+        $usuario = new User;
+        $usuario->name = $request->name;
+        $usuario->email = $request->email;
+        $usuario->password = bcrypt($request->password);
+        $usuario->save();
+
+        // responder
+        return response()->json(["mensaje" => "Usuario Registrado"], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+
+        return response()->json($user, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // validar
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users,email,$id",
+            "password" => "required"
+        ]);
+
+        // guardar
+        $usuario = User::find($id);
+        $usuario->name = $request->name;
+        $usuario->email = $request->email;
+        $usuario->password = bcrypt($request->password);
+        $usuario->update();
+
+        // responder
+        return response()->json(["mensaje" => "Usuario Modificado"], 201);
+   
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $usuario = User::find($id);
+        $usuario->delete();
+        return response()->json(["mensaje" => "Usuario Eliminado"], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,3 +26,8 @@ Route::group(["prefix" => "v1/auth"], function(){
 });
 
 // otras rutas
+Route::group(["middleware" => "auth:sanctum"], function(){
+    
+    Route::apiResource("usuario", UsuarioController::class);
+
+});
